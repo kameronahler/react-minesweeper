3a493d326f66e302d9bfc2e7150ee738a4840a12
Kameron Ahler <kamahler@gmail.com>
2020-08-30 17:45:38 -0600

    add minor styling to tiles

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -5,7 +5,13 @@ export default function Field() {
   const globalState = useContext(GameContext)
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 100px' }}>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(4, 50px',
+        gridTemplateRows: 'repeat(4,50px',
+      }}
+    >
       {globalState.tiles.map((el, position) => {
         return <button key={position}>{el.isBomb ? 'true' : 'false'}</button>
       })}
