594b0d039da489fc489ae1b3b6d77f8b2f30f8fd
Kameron Ahler <kamahler@gmail.com>
2020-08-30 20:19:27 -0600

    calculate whether tiles are on edges

--- src/store/GlobalStateWrapper.js
+++ src/store/GlobalStateWrapper.js
@@ -1,27 +1,68 @@
 import React from 'react'
 import TestData from './test.json'
 
-const initialTiles = TestData[0].test.map((el, i) => {
-  return { position: i + 1, isBomb: el }
+export const GameContext = React.createContext()
+
+const LEFT_COLUMN = edgeHelper(1, 16, 4)
+
+const RIGHT_COLUMN = edgeHelper(4, 16, 4)
+
+const INTIAL_TILE_DATA = TestData[0].test.map((el, i) => {
+  return {
+    position: {
+      index: i + 1,
+      edge: {
+        top: checkTop(i + 1, 16, 4, 4),
+        bottom: checkBottom(i + 1, 16, 4, 4),
+        left: checkLeft(i + 1, 16, 4, 4),
+        right: checkRight(i + 1, 16, 4, 4),
+      },
+    },
+    isBomb: el,
+  }
 })
 
-export const GameContext = React.createContext()
+function edgeHelper(i, total, int) {
+  let targets = []
+
+  while (i <= total) {
+    targets.push(i)
+    i = i + int
+  }
+
+  return targets
+}
+
+function checkTop(index, rows) {
+  if (index >= 1 && index <= rows) {
+    return true
+  } else return false
+}
+
+function checkBottom(index, total, columns) {
+  if (index > total - columns) {
+    return true
+  } else return false
+}
+
+function checkLeft(index) {
+  return LEFT_COLUMN.find((el) => el === index) ? true : false
+}
+
+function checkRight(index) {
+  return RIGHT_COLUMN.find((el) => el === index) ? true : false
+}
 
 export function GlobalStateWrapper({ children }) {
   const globalState = {
     field: {
       x: 4,
       y: 4,
-      sum: 16,
+      sum: INTIAL_TILE_DATA.length,
     },
-    tiles: initialTiles,
+    tiles: INTIAL_TILE_DATA,
     status: {
       alive: true,
-    },
-    actions: {
-      clear: 'Cleared',
-      flag: 'Flagged',
-      restart: 'Restart',
     },
   }
 
