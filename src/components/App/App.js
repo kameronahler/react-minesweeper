5aa0f04632234886035b5a899e67f46b474440fd
Kameron Ahler <kamahler@gmail.com>
2020-09-07 12:20:29 -0600

    Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { GlobalStateWrapper } from '../../store/GlobalStateWrapper'
 
 // components
+import Header from '../Header/Header'
 import Field from '../Field/Field'
 
 export default function App() {
   return (
     <div>
-      <header>
-        <h1>Let's Play Minecraft</h1>
-      </header>
       <GlobalStateWrapper>
+        <Header />
         <Field />
       </GlobalStateWrapper>
     </div>
