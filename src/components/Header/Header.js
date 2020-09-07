46678a5f99da21f7bc3045d6ec6660b5605d4566
Kameron Ahler <kamahler@gmail.com>
2020-09-07 12:37:50 -0600

    conditional text for header based on alive/dead

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GameContext } from '../../store/GlobalStateWrapper'
 
 export default function Header() {
+  const [field] = useContext(GameContext)
   return (
     <header>
-      <h1>Let's Play Minecraft</h1>
+      <h1>
+        {field.alive
+          ? `Let's play Minecraft`
+          : 'When did you get so good at Minecraft?'}
+      </h1>
     </header>
   )
 }
