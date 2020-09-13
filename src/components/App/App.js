b33b44394b05c10088973659f62a991e572f27fc
Kameron <kamahler@gmail.com>
2020-09-13 11:20:57 -0600

    unused import and param

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { GlobalStateWrapper } from '../../store/GlobalStateWrapper'
 import Header from '../Header/Header'
-import NewGameForm from '../NewGameForm/NewGameForm'
 import Field from '../Field/Field'
 import './App.scss'
 
